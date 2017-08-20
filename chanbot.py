7ceb596606cbaa31ef5ff31e9644b6a0f7edea04
toxicologist <toxiccologist@gmail.com>
2017-08-20 14:25:36 +0000

    Fixed nonetype error if posters had no name

--- chanbot.py
+++ chanbot.py
@@ -17,20 +17,21 @@ while True:
 	allthreads = board.get_all_thread_ids()
 	for x in range(len(allthreads)):
 		thread = board.get_thread(allthreads[x])
-		posts = thread.all_posts
-	#	print(x)
+		posts = thread.posts
+#		print("Scanning", thread)
 		for i in range(len(posts)):
 			currentpost = posts[i]
 			currentnum = currentpost.post_number
-	#		if "$4CHN:" in currentpost.name and currentpost.name not in open("bot.txt","r").read():
-			if "$4CHN:" in currentpost.name and str(currentnum) not in open("bot.txt", "r").read() and has_doubles(currentnum,2):
+#			if "$4CHN:" in currentpost.name and currentpost.name not in open("bot.txt","r").read():
+			if type(currentpost.name) == str and "$4CHN:" in currentpost.name and str(currentnum) not in open("bot.txt", "r").read() and has_doubles(currentnum,2):
 				addy = currentpost.name.replace("$4CHN:", "")
 				print("Found addy", addy, "with GET", currentnum, "in thread", thread)
-				send_coins(addy, 0.01)
+				send_coins(addy, 0.5)
 				print("Coins sent to ", addy)
 				print("------")
 				open("bot.txt","a").write(str(currentnum))
-			elif str(currentnum) in open("bot.txt", "r").read():
-				print("Found post ", currentnum, "but it is already in bot.txt. Skipping")
-				print("-------")
-	print("Scanning again..")
+#			elif str(currentnum) in open("bot.txt", "r").read():
+#				print("Found post ", currentnum, "but it is already in bot.txt. Skipping")
+#				print("-------")
+#	print("Scanning again..")
+
